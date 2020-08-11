5eb59c6f8e1c205ae7eee1a36c5309f3dfcc8cff
dguzmaneygt44 <djguzman_arena@hotmail.com>
2020-08-10 20:43:22 -0600

    Se agregó el modelo Curso.java

---
+++ src/main/java/gt/edu/umg/as2p1/model/Curso.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gt.edu.umg.as2p1.model;
+
+/**
+ *
+ * @author AK272DT
+ */
+public class Curso {
+    private Integer id_course;
+    private String name;
+    private String description;
+    
+    public Curso() {}
+    
+    public Curso(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+    
+    public Curso(Integer id_course, String name, String description){
+        this.id_course = id_course;
+        this.name = name;
+        this.description = description;
+    }
+    
+    public Integer getId(){
+        return id_course;
+    }
+    
+    public void setId(Integer id){
+        this.id_course = id;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getDescription(){
+        return description;
+    }
+    
+    public void setDescription(String description){
+        this.description = description;
+    }
+}
