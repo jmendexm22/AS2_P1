b661b204c3d897ce8351f214e2fc9e88a1569f66
dguzmaneygt44 <djguzman_arena@hotmail.com>
2020-08-12 11:54:18 -0600

    Se agregó el controlador EstudianteController con toda la lógica; se modificó el template estudiantes.html

---
+++ src/main/java/gt/edu/umg/as2p1/controller/EstudianteController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gt.edu.umg.as2p1.controller;
+
+import gt.edu.umg.as2p1.dao.EstudianteDAOImpl;
+import gt.edu.umg.as2p1.model.EstudianteEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ *
+ * @author AK272DT
+ */
+
+@Controller
+public class EstudianteController {
+    
+    @GetMapping("/getEstudiante")
+    public String getEstudiante(Model model) {
+        model.addAttribute("estudiante", new EstudianteEntity());
+        return "estudiantes";
+    } 
+    
+    @PostMapping("/postEstudiante")
+    public String postEstudiante(@ModelAttribute EstudianteEntity estudiante, Model model) {     
+        
+        EstudianteDAOImpl estudianteDAO = new EstudianteDAOImpl();
+        boolean status = estudianteDAO.insertarEstudiante(estudiante);
+        
+        String mensaje = (status) ? "Insertado correctamente!" : "No se pudo insertar el registro...";
+
+        model.addAttribute("mensaje", mensaje);
+        
+        return "index";
+    }
+    
+}
