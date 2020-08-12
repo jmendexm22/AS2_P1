c8501f87e51895e629ecab9f3c9e6b1668eb04a3
jmendexm22 <jmendez22111994@gmail.com>
2020-08-12 02:58:44 -0600

    clase conexion

---
+++ src/main/java/gt/edu/umg/as2p1/service/ConnectionFactory.java
@@ -0,0 +1,47 @@
+package gt.edu.umg.as2p1.service;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	
+    public static final String URL = "jdbc:mysql://192.185.4.65:3306/jbarilla_ingsoftware?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+    public static final String USER = "jbarilla_estudia";
+
+    public static final String PASS = "2Ui!OssHDQGv";
+
+    public static Connection getConnection()
+
+    {
+
+      try {
+
+         // DriverManager.registerDriver(new Driver());
+
+          return DriverManager.getConnection(URL, USER, PASS);
+
+      } catch (SQLException ex) {
+
+          throw new RuntimeException("Error connecting to the database", ex);
+
+      }
+
+    }
+
+    /**
+
+     * Test Connection
+
+     */
+
+    public static void main(String[] args) {
+
+        ConnectionFactory connectionFactory;
+		Connection connection = ConnectionFactory.getConnection();
+
+    }
+
+}
